4172dd74707d8ad88a27273c6f88affdc34ae5a6
Yanaira Ponce <yanairap31929@students.laalliance.org>
2017-11-17 20:51:06 +0000

    Looping/checkerboard finished.

---
+++ sketch.js
@@ -0,0 +1,97 @@
+function setup() {
+  createCanvas(500,400)
+}
+
+function draw() {
+  background(0)
+  fill(250)
+    var y = 100
+    var x = 50;
+    
+  for ( var i = 0; i < 8; i++){
+    if ( i % 2 === 0) {
+      fill ('purple')// even
+    } else {
+      fill ('white')// odd
+    }
+  rect(x, y, 40, 40);
+  x += 40;
+  }
+    var x = 50;
+    
+  for ( var i = 0; i < 8; i++){
+    if ( i % 2 === 0) {
+      fill ('white')// even
+    } else {
+      fill ('purple')// odd
+    }
+  rect(x, y + 40, 40, 40);
+  x += 40;
+  }
+      var x = 50;
+    
+  for ( var i = 0; i < 8; i++){
+    if ( i % 2 === 0) {
+      fill ('purple')// even
+    } else {
+      fill ('white')// odd
+    }
+  rect(x, y + 80, 40, 40);
+  x += 40;
+  }
+  var x = 50;
+    
+  for ( var i = 0; i < 8; i++){
+    if ( i % 2 === 0) {
+      fill ('white')// even
+    } else {
+      fill ('purple')// odd
+    }
+  rect(x, y + 120, 40, 40);
+  x += 40;
+  }
+  var x = 50;
+    
+  for ( var i = 0; i < 8; i++){
+    if ( i % 2 === 0) {
+      fill ('purple')// even
+    } else {
+      fill ('white')// odd
+    }
+  rect(x, y + 160, 40, 40);
+  x += 40;
+  }
+  var x = 50;
+    
+  for ( var i = 0; i < 8; i++){
+    if ( i % 2 === 0) {
+      fill ('white')// even
+    } else {
+      fill ('purple')// odd
+    }
+  rect(x, y + 200, 40, 40);
+  x += 40;
+  }
+  var x = 50;
+    
+  for ( var i = 0; i < 8; i++){
+    if ( i % 2 === 0) {
+      fill ('purple')// even
+    } else {
+      fill ('white')// odd
+    }
+  rect(x, y + 240, 40, 40);
+  x += 40;
+  }
+  var x = 50;
+    
+  for ( var i = 0; i < 8; i++){
+    if ( i % 2 === 0) {
+      fill ('white')// even
+    } else {
+      fill ('purple')// odd
+    }
+  rect(x, y - 40, 40, 40);
+  x += 40;
+  }
+}
